7e929b43fcee87739baeddb39e90784a71b9b943
pavankumarNM <54441613+pavankumarNM@users.noreply.github.com>
2022-06-27 21:00:54 +0530

    Update protectedexample.cpp

--- protectedexample.cpp
+++ protectedexample.cpp
@@ -12,7 +12,7 @@ class derived:protected base{
     public:
     print(void){
      cout<<"printing in class  base"<<a<<endl;
-     //cout<<"printing in class  base"<<b<<endl;     cant ptint because private there cant be inherited
+     //cout<<"printing in class  base"<<b<<endl;     cant print because private there cant be inherited
      cout<<"printing in class  base"<<c<<endl;
       }
 
